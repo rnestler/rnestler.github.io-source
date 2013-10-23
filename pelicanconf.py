c228c307a918faefe4e843ab09e4293f1f212686
Raphael Nestler <raphael.nestler@gmail.com>
2013-10-23 14:22:22 +0200

    added link to coredump.ch

--- pelicanconf.py
+++ pelicanconf.py
@@ -19,7 +19,7 @@ TRANSLATION_FEED_ATOM = None
 LINKS =  (('Pelican', 'http://getpelican.com/'),
           ('Python.org', 'http://python.org/'),
           ('Jinja2', 'http://jinja.pocoo.org/'),
-          ('You can modify those links in your config file', '#'),)
+          ('coredump', 'http://www.coredump.ch/'))
 
 # Social widget
 SOCIAL = (('You can add links in your config file', '#'),
